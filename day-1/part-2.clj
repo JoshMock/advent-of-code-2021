d46fa69389b8d615e2ee9dc7af91daaac56ce25a
Josh Mock <josh@joshmock.com>
2021-12-07 16:35:11 -0600

    day 1 part 2 WIP again
    I just realized I misread the question, so I'll have some more work to
    do to finish this. :facepalm:

--- day-1/part-2.clj
+++ day-1/part-2.clj
@@ -1,11 +1,17 @@
 #!/usr/bin/env bb
+(ns part-2
+  (:require [clojure.string :as str]))
 
 (defn reducer [old new]
-  (let [meas-val (first new)
-        meas-names (rest new)]
-    (assoc old meas-val meas-names)))
+  (let [meas-val (Integer/parseInt (first new))
+        meas-names (rest new)
+        pivot (zipmap meas-names (repeat [meas-val]))]
+    (merge-with concat old pivot)))
+
+(defn lt [[[_ a] [_ b]]] (if (< a b) 1 0))
 
 (let [lines (str/split (slurp "./day-1/part-2.txt") #"\n")
       lines (map #(str/split % #"\s+") lines)
-      data (reduce reducer {} lines)]
-  (println data))
+      pivoted (reduce reducer {} lines)
+      summed (map (fn [[k v]] [k (apply + v)]) pivoted)]
+  (println (apply + (map lt (partition 2 1 summed)))))
