1209722b5d3f60cb2ccb3dbd92efb1ba63bb5373
Josh Mock <josh@joshmock.com>
2021-12-07 22:40:01 -0600

    Finished day 1

--- day-1/part-2.clj
+++ day-1/part-2.clj
@@ -2,16 +2,10 @@
 (ns part-2
   (:require [clojure.string :as str]))
 
-(defn reducer [old new]
-  (let [meas-val (Integer/parseInt (first new))
-        meas-names (rest new)
-        pivot (zipmap meas-names (repeat [meas-val]))]
-    (merge-with concat old pivot)))
+(defn lt [[a b]] (if (< a b) 1 0))
 
-(defn lt [[[_ a] [_ b]]] (if (< a b) 1 0))
-
-(let [lines (str/split (slurp "./day-1/part-2.txt") #"\n")
-      lines (map #(str/split % #"\s+") lines)
-      pivoted (reduce reducer {} lines)
-      summed (map (fn [[k v]] [k (apply + v)]) pivoted)]
-  (println (apply + (map lt (partition 2 1 summed)))))
+(let [lines (str/split (slurp "./day-1/part-1.txt") #"\n")
+      measures (map #(Integer/parseInt %) lines)
+      windows (partition 3 1 measures)
+      totals (map #(apply + %) windows)]
+  (println (apply + (map lt (partition 2 1 totals)))))
