d373af99140fb094e33fb74a9b3ef478fecfed1a
Josh Mock <josh@joshmock.com>
2021-12-16 16:38:12 -0600

    Day 3 part 2 WIP

--- day-3/part-2.clj
+++ day-3/part-2.clj
@@ -21,14 +21,19 @@
   (let [x (atom cmds)
         idx (atom 0)]
     (while (> (count @x) 1)
-      (swap! x #(filter #(= (nth % idx) (nth a idx)) @x))
-      (swap! idx inc)
-      (prn (count @x))
-      (prn @idx))
-    @x))
+      (reset! x (filter #(= (Integer/parseInt (nth % @idx)) (nth a @idx)) @x))
+      (swap! idx inc))
+    (first @x)))
 
 (let [cmds (map #(str/split % #"") (str/split (slurp "./day-3/part-1.txt") #"\n"))
       most (most-common cmds)
-      least (least-common cmds)]
-  (prn most)
-  (prn (count (similar most cmds))))
+      least (least-common cmds)
+      most-sim-bin (apply str (similar most cmds))
+      most-sim-int (Integer/parseInt most-sim-bin 2)
+      least-sim-bin (apply str (similar least cmds))
+      least-sim-int (Integer/parseInt least-sim-bin 2)]
+  (prn (apply str most))
+  (prn most-sim-bin)
+  (prn (apply str least))
+  (prn least-sim-bin)
+  (prn (* most-sim-int least-sim-int)))
